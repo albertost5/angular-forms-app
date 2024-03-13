9d13280bd9c8eef2446891d03c7ddb2ead42fa61
albertost5 <sancheztorresalberto@gmail.com>
2024-03-13 19:10:59 +0100

    Added routing and created side-menu component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SideMenuComponent} from './components/side-menu/side-menu.component';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideMenuComponent
+  ],
+  exports: [
+    SideMenuComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
