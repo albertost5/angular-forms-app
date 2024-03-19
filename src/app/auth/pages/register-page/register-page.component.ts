ca721c6b2d2fb81df8e756d2cab82bfe7bd2169b
albertost5 <sancheztorresalberto@gmail.com>
2024-03-19 01:57:07 +0100

    Added email validator service, adapted service validator and registerPage html

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
 // import * as customValidators from '../../../shared/validators/validators';
 import {ValidatorsService} from '../../../shared/services/validators.service';
+import {EmailValidatorService} from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -12,7 +13,8 @@ export class RegisterPageComponent {
 
   constructor(
     private readonly fb: FormBuilder,
-    private readonly validatorsService: ValidatorsService
+    private readonly validatorsService: ValidatorsService,
+    private readonly emailValidatorService: EmailValidatorService
   ) {
   }
 
@@ -20,11 +22,16 @@ export class RegisterPageComponent {
     // name: ['', [Validators.required, Validators.pattern( customValidators.firstNameAndLastnamePattern )]],
     name: ['', [Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern )]],
     // email: ['', [Validators.required, Validators.email]],
-    email: ['', [Validators.required, Validators.pattern( this.validatorsService.emailPattern )]],
+    // email: ['', [Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ new EmailValidatorService()]],
+    email: ['', [Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ this.emailValidatorService ]],
     // username: ['', [Validators.required, customValidators.cantBeDefaultUsername]],
     username: ['', [Validators.required, this.validatorsService.cantBeDefaultUsername]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required, Validators.minLength(6)]],
+  }, {
+    validators: [
+      this.validatorsService.isFieldOneEqualFieldTwo('password', 'password2')
+    ]
   });
 
   public isInvalidField(formField: string): boolean {
--- src/app/shared/services/validators.service.ts
+++ src/app/shared/services/validators.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import {FormControl, FormGroup, ValidationErrors} from '@angular/forms';
+import {Injectable} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors} from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +22,20 @@ export class ValidatorsService {
 
   public isInvalidField(form: FormGroup, formField: string): boolean {
     return !!form.controls[formField].errors && form.controls[formField].touched;
+  }
+
+  public isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const fieldValue1 = formGroup.get(field1)?.value;
+      const fieldValue2 = formGroup.get(field2)?.value;
+
+      if (fieldValue1 !== fieldValue2) {
+        formGroup.get(field2)?.setErrors({notEqual: true});
+        return {notEqual: true}
+      }
+
+      formGroup.get(field2)?.setErrors(null);
+      return null;
+    }
   }
 }
